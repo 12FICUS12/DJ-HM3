44c6f844e18524ff57fa1661ab8b06e2fdb9f55c
12FICUS12 <maria.Alekseeva14@mail.com>
2024-06-10 16:44:09 +0300

    Initial commit2

--- work_with_database/phones/views.py
+++ work_with_database/phones/views.py
@@ -9,21 +9,23 @@ def index(request):
 
 def show_catalog(request):
     catalog_ob = Phone.objects.all()
-    sort_by = request.GET.get('sort_by')
+    sort_by = request.GET.get('sort')
     if sort_by == 'max_price':
-        catalog_ob = catalog_ob.order_by('price').reverse()
+        catalog_ob = catalog_ob.order_by('-price')
     elif sort_by == 'min_price':
         catalog_ob = catalog_ob.order_by('price')
     elif sort_by == 'name':
         catalog_ob = catalog_ob.order_by('name')
 
 
-    return render(request, 'catalog.html', {'catalog': catalog_ob})
+    context = {'phones': catalog_ob}
+
+    return render(request, 'catalog.html', context=context)
 
 
 
 def show_product(request, slug):
     product_ob = Phone.objects.get(slug=slug)
-    return render(request, 'product.html', {'product': product_ob})
+    return render(request, 'product.html', {'phone': product_ob})
 
 
